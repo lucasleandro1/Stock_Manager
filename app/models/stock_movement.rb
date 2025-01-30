7b32f40cd44c43ee8de4971cf6dc29f012f87c95
Lucas Leandro <lukkassoliveira2215@gmail.com>
2025-01-30 17:02:26 -0300

    test

--- app/models/stock_movement.rb
+++ app/models/stock_movement.rb
@@ -2,5 +2,5 @@ class StockMovement < ApplicationRecord
   belongs_to :product
 
   attribute :movement_type, :integer, default: 0
-  enum movement_type: { entrada: 0, saida: 1 }
+  enum :movement_type, entrada: 0, saida: 1
 end
