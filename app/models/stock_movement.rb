3717ccb68b8a3846ae42340ec73246f586fbd0cf
Lucas Leandro <lukkassoliveira2215@gmail.com>
2025-01-30 13:09:56 -0300

    docker

--- app/models/stock_movement.rb
+++ app/models/stock_movement.rb
@@ -1,6 +1,5 @@
 class StockMovement < ApplicationRecord
   belongs_to :product
 
-  enum movement_type: { entrada: 0, saida: 1 }
-  enum reason: { venda: 0, reposição: 1 }
+  enum movement_type: { "entrada" => 0, "saida" => 1}
 end
