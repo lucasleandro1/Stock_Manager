1a74606b38a40639312f6b636c58792d796843df
Lucas Leandro <lukkassoliveira2215@gmail.com>
2025-02-16 18:09:59 -0300

    Graphic in dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,15 +5,19 @@ class HomeController < ApplicationController
     @products = Product.all
     @movimentacoes_recentes = StockMovement.order(created_at: :desc).limit(5)
 
-
     @vendas_por_mes = StockMovement
     .where(created_at: 1.year.ago..Time.current)
     .group_by_month(:created_at, format: "%B %Y")
     .sum(:movement_type)
 
-    @lucro_por_mes = StockMovement
+    @lucro = StockMovement
       .where(created_at: 1.year.ago..Time.current)
       .group_by_month(:created_at, format: "%B %Y")
       .sum { |movimentacao|movimentacao.product.lucro_total }
+      
+
+      @lucro_por_mes = StockMovement.lucro_por_mes.to_h
+
+
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+import "chartkick/chart.js";
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+
+document.addEventListener("turbo:load", function () {
+  console.log("Turbo carregado!");
+});
